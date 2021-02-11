d37f58e1cd82eed49788e4900f7954692861388f
Ulf Norell <ulf.norell@gmail.com>
2021-02-11 10:57:49 +0100

    QuickCheck model for auction contract

--- plutus-use-cases/test/Spec/Auction.hs
+++ plutus-use-cases/test/Spec/Auction.hs
@@ -1,22 +1,31 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
-module Spec.Auction(tests, auctionTrace1, auctionTrace2) where
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE TypeFamilies       #-}
+module Spec.Auction(tests, auctionTrace1, auctionTrace2,
+                    prop_Auction, prop_FinishAuction) where
 
 import           Control.Lens
-import           Control.Monad                                    (void)
+import           Control.Monad                                    (void, when)
+import           Control.Monad.Freer.Log                          (LogLevel (..))
 
-import           Language.Plutus.Contract
-import           Language.Plutus.Contract.Test
-import           Ledger                                           (Ada, Value, pubKeyHash)
+import           Language.Plutus.Contract                         hiding (currentSlot, when)
+import           Language.Plutus.Contract.Test                    hiding (not)
+import           Ledger                                           (Ada, Slot (..), Value, pubKeyHash)
 import qualified Ledger.Ada                                       as Ada
 
 import qualified Language.Plutus.Contract.StateMachine            as SM
-import           Language.PlutusTx.Coordination.Contracts.Auction
+import           Language.Plutus.Contract.Test.ContractModel
+import           Language.PlutusTx.Coordination.Contracts.Auction hiding (Bid)
 import           Language.PlutusTx.Monoid                         (inv)
 import qualified Ledger.Value                                     as Value
 import qualified Plutus.Trace.Emulator                            as Trace
 
+import           Test.QuickCheck                                  hiding ((.&&.))
 import           Test.Tasty
 
 tests :: TestTree
@@ -80,7 +89,7 @@ auctionTrace1 = do
     _ <- Trace.activateContractWallet w1 seller
     _ <- Trace.waitNSlots 1
     hdl2 <- Trace.activateContractWallet w2 buyer
-    _ <- Trace.waitNSlots 1
+    _ <- Trace.waitNSlots 1     -- waiting an even multiple of 3 here makes test fail!
     Trace.callEndpoint @"bid" hdl2 50
     void $ Trace.waitUntilSlot (succ $ succ $ apEndTime params)
 
@@ -100,3 +109,119 @@ auctionTrace2 = do
     _ <- Trace.waitNSlots 35
     Trace.callEndpoint @"bid" hdl2 70
     void $ Trace.waitUntilSlot (succ $ succ $ apEndTime params)
+
+-- * QuickCheck model
+
+data AuctionModel = AuctionModel
+    { _currentBid :: Integer
+    , _winner     :: Wallet
+    , _endSlot    :: Slot
+    , _phase      :: Phase }
+    deriving (Show)
+
+data Phase = NotStarted | Bidding | AuctionOver
+    deriving (Eq, Show)
+
+makeLenses 'AuctionModel
+
+deriving instance Eq (HandleKey AuctionModel s)
+deriving instance Show (HandleKey AuctionModel s)
+
+instance ContractModel AuctionModel where
+
+    type Err AuctionModel = SM.SMContractError
+
+    data HandleKey AuctionModel s where
+        SellerH :: HandleKey AuctionModel SellerSchema
+        BuyerH  :: Wallet -> HandleKey AuctionModel BuyerSchema
+
+    data Command AuctionModel = Init | Bid Wallet Integer | WaitUntil Slot
+        deriving (Eq, Show)
+
+    initialState = AuctionModel { _currentBid = 0
+                                , _winner     = w1
+                                , _endSlot    = apEndTime params
+                                , _phase      = NotStarted }
+
+    arbitraryCommand s
+        | p /= NotStarted =
+            oneof [ WaitUntil . step <$> choose (1, 10 :: Integer)
+                  , Bid  <$> (Wallet <$> choose (2, 4)) <*> choose (1, 1000) ]
+        | otherwise = pure Init
+        where
+            p    = s ^. modelState . phase
+            slot = s ^. currentSlot
+            step n = slot + fromIntegral n
+
+    precondition s Init = s ^. modelState . phase == NotStarted
+    precondition s cmd  = s ^. modelState . phase /= NotStarted &&
+        case cmd of
+            WaitUntil slot -> slot > s ^. currentSlot
+            _              -> True
+
+    -- This command is only for setting up the model state with theToken
+    nextState cmd = do
+        slot <- viewState currentSlot
+        end  <- viewModelState endSlot
+        case cmd of
+            Init -> do
+                phase $= Bidding
+                withdraw w1 theToken
+                wait 3
+            WaitUntil slot -> waitUntil slot
+            Bid w bid -> do
+                current <- viewModelState currentBid
+                leader  <- viewModelState winner
+                wait 1
+                when (bid > current && slot <= end) $ do
+                    withdraw w $ Ada.lovelaceValueOf bid
+                    deposit leader $ Ada.lovelaceValueOf current
+                    currentBid $= bid
+                    winner     $= w
+        slot' <- viewState currentSlot
+        p     <- viewModelState phase
+        when (slot' > end && p == Bidding) $ do
+            w   <- viewModelState winner
+            bid <- viewModelState currentBid
+            phase $= AuctionOver
+            deposit w theToken
+            deposit w1 $ Ada.lovelaceValueOf bid
+
+    perform _ Init = delay 3
+    perform _ (WaitUntil slot) = void $ Trace.waitUntilSlot slot
+    perform s (Bid w bid) = do
+        Trace.callEndpoint @"bid" (handle s $ BuyerH w) (Ada.lovelaceOf bid)
+        delay 1
+
+    shrinkCommand _ Init      = []
+    shrinkCommand _ (WaitUntil (Slot n))  = [ WaitUntil (Slot n') | n' <- shrink n ]
+    shrinkCommand s (Bid w v) =
+        [ WaitUntil (s ^. currentSlot + 1) ] ++
+        [ Bid w v' | v' <- shrink v ]
+
+    monitoring _ _ = id
+
+delay :: Integer -> Trace.EmulatorTrace ()
+delay n = void $ Trace.waitNSlots $ fromIntegral n
+
+prop_Auction :: Script AuctionModel -> Property
+prop_Auction script =
+    propRunScriptWithOptions (set minLogLevel Info options) spec
+        (\ _ -> pure True)  -- TODO: check termination
+        (\ _ -> pure ())
+        script
+        (\ _ -> pure ())
+    where
+        spec = HandleSpec SellerH w1 seller :
+               [ HandleSpec (BuyerH w) w buyer | w <- map Wallet [2..4] ]
+
+finishAuction :: DL AuctionModel ()
+finishAuction = do
+    action Init
+    anyActions_
+    slot <- viewState currentSlot
+    when (slot < 101) $ action $ WaitUntil 101
+    assertModel (Value.isZero . lockedFunds)
+
+prop_FinishAuction :: Property
+prop_FinishAuction = forAllDL finishAuction prop_Auction
