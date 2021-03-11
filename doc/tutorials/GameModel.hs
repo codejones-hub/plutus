e17da405103e02f8026d638e48cba6445a851774
John <john.hughes@quviq.com>
2021-03-11 16:40:23 +0100

    Update GameModel.gs

--- doc/tutorials/GameModel.hs
+++ doc/tutorials/GameModel.hs
@@ -1,170 +1,228 @@
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE DeriveFunctor        #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE GADTs                #-}
-{-# LANGUAGE MonoLocalBinds       #-}
-{-# LANGUAGE MultiWayIf           #-}
-{-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE RankNTypes           #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE StandaloneDeriving   #-}
-{-# LANGUAGE TemplateHaskell      #-}
-{-# LANGUAGE TypeApplications     #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE UndecidableInstances #-}
-module Spec.GameStateMachine where
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveFunctor      #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE TypeFamilies       #-}
 
-import           Control.Applicative
-import           Control.Lens
+module GameStateMachineModel where
+
+import           Control.Lens                                              hiding (elements)
 import           Control.Monad
-import           Control.Monad.Freer.Error
 import           Control.Monad.Freer.Log                                   (LogLevel (..))
-import           Control.Monad.Freer.Writer
-import           Data.List
-import           Data.Map                                                  (Map)
-import qualified Data.Map                                                  as Map
 import           Data.Maybe
-import           Data.Text.Prettyprint.Doc
-import           Data.Void
--- START_IMPORT_QC
-import           Test.QuickCheck                                           as QC hiding ((.&&.))
--- END_IMPORT_QC
-import           Test.Tasty                                                hiding (after)
-import qualified Test.Tasty.HUnit                                          as HUnit
+import           Test.QuickCheck
 
-import qualified Spec.Lib                                                  as Lib
-
-import qualified Language.PlutusTx                                         as PlutusTx
-
--- START_IMPORT_CONTRACT_TEST
 import           Language.Plutus.Contract.Test
--- END_IMPORT_CONTRACT_TEST
--- START_IMPORT_CONTRACT_MODEL
 import           Language.Plutus.Contract.Test.ContractModel
--- END_IMPORT_CONTRACT_MODEL
-import           Language.Plutus.Contract.Test.StateModel                  (stateAfter)
-import qualified Language.Plutus.Contract.Test.StateModel                  as StateModel
 import           Language.PlutusTx.Coordination.Contracts.GameStateMachine as G
-import           Language.PlutusTx.Lattice
-import qualified Ledger.Typed.Scripts                                      as Scripts
-import           Ledger.Value                                              (Value, isZero)
-import           Plutus.Trace.Emulator                                     as Trace
-import           Wallet.Emulator                                           (Wallet)
-import           Wallet.Emulator.Folds                                     (postMapM)
-
--- START_IMPORTEMULATOR
-import           Plutus.Trace.Emulator                                     as Trace
--- END_IMPORTEMULATOR
-
--- START_ADAIMPORTS
 import qualified Ledger.Ada                                                as Ada
-import           Ledger.Value
--- END_ADAIMPORTS
--- START_SCRIPTSIMPORT
 import qualified Ledger.Typed.Scripts                                      as Scripts
--- END_SCRIPTSIMPORT
+import           Ledger.Value
+import           Plutus.Trace.Emulator                                     as Trace
 
 -- * QuickCheck model
 
--- START_MODELSTATE
 data GameModel = GameModel
     { _gameValue     :: Integer
     , _hasToken      :: Maybe Wallet
     , _currentSecret :: String }
-    deriving (Show)
+  deriving (Show)
 
 makeLenses 'GameModel
--- END_MODELSTATE
 
+deriving instance Eq (HandleKey GameModel schema err)
+deriving instance Show (HandleKey GameModel schema err)
 
--- START_DEFINE_WALLETS
-w1, w2, w3 :: Wallet
-w1 = Wallet 1
-w2 = Wallet 2
-w3 = Wallet 3
-
-wallets = [w1, w2, w3]
--- END_DEFINE_WALLETS
-
--- START_INSTANCE
 instance ContractModel GameModel where
 
-    data Action GameModel = Lock      Wallet String Integer
-                          | Guess     Wallet String String Integer
-                          | GiveToken Wallet
-        deriving (Eq, Show)
--- END_INSTANCE
-
--- START_HANDLE_KEY
     data HandleKey GameModel schema err where
         WalletKey :: Wallet -> HandleKey GameModel GameStateMachineSchema GameError
--- END_HANDLE_KEY
 
--- START_ARBITRARY
-    arbitraryAction s = oneof $
-        [ Lock      <$> genWallet <*> genGuess <*> genValue              ] ++
-        [ Guess     <$> genWallet <*> genGuess <*> genGuess <*> genValue ] ++
-        [ GiveToken <$> genWallet                                        ]
--- END_ARBITRARY
+    -- The commands available to a test case
+    data Action GameModel = Lock      Wallet String Integer
+                           | Guess     Wallet String String Integer
+                           | GiveToken Wallet
+        deriving (Eq, Show)
 
--- START_INITSTATEDEFS
     initialState = GameModel
         { _gameValue     = 0
         , _hasToken      = Nothing
         , _currentSecret = ""
         }
--- END_INITSTATEDEFS
 
+    -- 'perform' gets a state, which includes the GameModel state, but also contract handles for the
+    -- wallets and what the model thinks the current balances are.
+    perform handle s cmd = case cmd of
+        Lock w new val -> do
+            callEndpoint @"lock" (handle $ WalletKey w)
+                         LockArgs{lockArgsSecret = new, lockArgsValue = Ada.lovelaceValueOf val}
+            delay 2
+        Guess w old new val -> do
+            callEndpoint @"guess" (handle $ WalletKey w)
+                GuessArgs{ guessArgsOldSecret = old
+                         , guessArgsNewSecret = new
+                         , guessArgsValueTakenOut = Ada.lovelaceValueOf val}
+            delay 1
+        GiveToken w' -> do
+            let w = fromJust (s ^. contractState . hasToken)
+            payToWallet w w' gameTokenVal
+            return ()
+            delay 1
+
+    -- 'nextState' descibes how each command affects the state of the model
     nextState (Lock w secret val) = do
-        hasToken      $= Just w
-        currentSecret $= secret
-        gameValue     $= val
-        forge gameTokenVal
-        deposit  w gameTokenVal
+        wasUnlocked <- (Nothing ==) <$> viewContractState hasToken
+        when wasUnlocked $ do
+          hasToken      $= Just w
+          currentSecret $= secret
+          gameValue     $= val
+          forge gameTokenVal
+          deposit  w gameTokenVal
         withdraw w $ Ada.lovelaceValueOf val
+        wait 2
 
     nextState (Guess w old new val) = do
         correctGuess <- (old ==)    <$> viewContractState currentSecret
         holdsToken   <- (Just w ==) <$> viewContractState hasToken
-        enough       <- (>= val)    <$> viewContractState gameValue
-        when (correctGuess && holdsToken && enough) $ do
+        enoughAda    <- (val <=)    <$> viewContractState gameValue
+        when (correctGuess && holdsToken && enoughAda) $ do
             currentSecret $= new
             gameValue     $~ subtract val
             deposit w $ Ada.lovelaceValueOf val
+        wait 1
 
     nextState (GiveToken w) = do
         w0 <- fromJust <$> viewContractState hasToken
         transfer w0 w gameTokenVal
         hasToken $= Just w
+        wait 1
 
--- START_GENERATORS
+    -- To generate a random test case we need to know how to generate a random
+    -- command given the current model state.
+    arbitraryAction s = oneof $
+        [ Lock      <$> genWallet <*> genGuess <*> genValue ] ++
+        [ Guess w   <$> genGuess  <*> genGuess <*> choose (0, val)
+        | Just w <- [tok] ] ++
+        [ GiveToken <$> genWallet ]
+        where
+            tok = s ^. contractState . hasToken
+            val = s ^. contractState . gameValue
+
+
+    -- The 'precondition' says when a particular command is allowed.
+    precondition s cmd = case cmd of
+            Lock _ _ v    -> tok == Nothing && v >= 0
+            Guess w _ _ v -> tok == Just w && v <= val
+            GiveToken w   -> tok /= Nothing
+        where
+            tok = s ^. contractState . hasToken
+            val = s ^. contractState . gameValue
+
+
+
+    shrinkAction _s (Lock w secret val) =
+        [Lock w' secret val | w' <- shrinkWallet w] ++
+        [Lock w secret val' | val' <- shrink val]
+    shrinkAction _s (GiveToken w) =
+        [GiveToken w' | w' <- shrinkWallet w]
+    shrinkAction _s (Guess w old new val) =
+        [Guess w' old new val | w' <- shrinkWallet w] ++
+        [Guess w old new val' | val' <- shrink val]
+
+    monitoring (s,_) (Guess w old new v) =
+      tabulate "Guesses"
+        [if old==s ^. contractState . currentSecret then "Right" else "Wrong"]
+    monitoring _ _ = id
+
+handleSpec :: [HandleSpec GameModel]
+handleSpec = [ HandleSpec (WalletKey w) w G.contract | w <- wallets ]
+
+-- | The main property. 'propRunScript_' checks that balances match the model after each test.
+prop_Game :: Script GameModel -> Property
+prop_Game script = propRunScript_ handleSpec script
+
+propGame' :: LogLevel -> Script GameModel -> Property
+propGame' l s = propRunScriptWithOptions
+                    (set minLogLevel l defaultCheckOptions)
+                    handleSpec
+                    (\ _ -> pure True)
+                    s
+
+wallets :: [Wallet]
+wallets = [w1, w2, w3]
+
 genWallet :: Gen Wallet
 genWallet = elements wallets
 
+shrinkWallet :: Wallet -> [Wallet]
+shrinkWallet w = [w' | w' <- wallets, w' < w]
+
 genGuess :: Gen String
-genGuess = elements ["hello", "secret", "hunter2", "*******"]
+genGuess = elements guesses
+
+guesses = ["hello", "secret", "hunter2", "*******"]
 
 genValue :: Gen Integer
 genValue = getNonNegative <$> arbitrary
--- END_GENERATORS
 
--- START_GAME_PROPERTY
-prop_Game :: Script GameModel -> Property
-prop_Game script = propRunScript_ handleSpec script
--- END_GAME_PROPERTY
+delay :: Int -> EmulatorTrace ()
+delay n = void $ waitNSlots (fromIntegral n)
 
--- START_HANDLE_SPEC
-handleSpec :: [HandleSpec GameModel]
-handleSpec = [ HandleSpec (WalletKey w) w G.contract | w <- wallets ]
--- END_HANDLE_SPEC
+-- Dynamic Logic ----------------------------------------------------------
 
+prop_UnitTest :: Property
+prop_UnitTest = withMaxSuccess 1 $ forAllDL unitTest prop_Game
 
+unitTest :: DL GameModel ()
+unitTest = do
+    val <- forAllQ $ chooseQ (3, 20)
+    action $ Lock w1 "hello" val
+    action $ GiveToken w2
+    action $ Guess w2 "hello" "new secret" 3
 
--- START_GAMETOKEN
+unitTest2 :: DL GameModel ()
+unitTest2 = do
+    unitTest
+    action $ GiveToken w3
+    action $ Guess w3 "new secret" "hello" 4
+
+unitTestFail :: DL GameModel ()
+unitTestFail = do
+    action $ Lock w1 "hello" 8
+    action $ GiveToken w2
+    action $ Guess w2 "hola" "new secret" 3
+
+noLockedFunds :: DL GameModel ()
+noLockedFunds = do
+    (w0,funds,pass) <- forAllQ (elementsQ wallets, chooseQ (1,10000), elementsQ guesses)
+    action $ Lock w0 pass funds
+    anyActions_
+    w <- forAllQ $ elementsQ wallets
+    secret <- viewContractState currentSecret
+    val    <- viewContractState gameValue
+    when (val > 0) $ do
+        monitor $ label "Unlocking funds"
+        action $ GiveToken w
+        action $ Guess w secret "" val
+    assertModel "Locked funds should be zero" $ isZero . lockedValue
+
+-- | Check that we can always get the money out of the guessing game (by guessing correctly).
+prop_NoLockedFunds :: Property
+prop_NoLockedFunds = forAllDL noLockedFunds prop_Game
+
+-- | Wallets and game token.
+
+w1, w2, w3 :: Wallet
+w1 = Wallet 1
+w2 = Wallet 2
+w3 = Wallet 3
+
 gameTokenVal :: Value
 gameTokenVal =
     let sym = Scripts.monetaryPolicyHash G.scriptInstance
     in G.token sym "guess"
--- END_GAMETOKEN
-
